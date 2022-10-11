1fed6c7fdb98816cd3bd0a9379a7312b9c4cc9c8
Symbocracy <115591647+Symbocracy@users.noreply.github.com>
2022-10-11 23:37:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const myInterval = setInterval(changeTitle, 1000, "mainTitle");
 let myIntervalCounter = 0;
 function changeTitle(mainTitle) {
   let title = getid(mainTitle);
-  let differentTitles = ["$1M80KR4C1", "Symbocracy", "Simbokreisi, "Simbokrasi", "$YM80KR4CY", "$¥M80CR4CY", "Simbocreisi"];
+  let differentTitles = ["$1M80KR4C1", "Symbocracy", "Simbokreisi", "Simbokrasi", "$YM80KR4CY", "$¥M80CR4CY", "Simbocreisi"];
 
   if (myIntervalCounter != differentTitles.length - 1) myIntervalCounter++;
   else myIntervalCounter = 0;
